86e29e530ffee0e532d69eee9c6fc3c143a4e451
Arjun Jagdale <142811259+ArjunJagdale@users.noreply.github.com>
2025-07-31 23:10:51 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -74,7 +74,7 @@ function App() {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          Authorization: `Bearer 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`
+          Authorization: `Bearer sk-##`
         },
         body: JSON.stringify({
           model: "mistralai/mistral-small-3.2-24b-instruct:free",
